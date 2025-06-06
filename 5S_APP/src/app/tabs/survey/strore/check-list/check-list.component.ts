c6b3b2cad35fb9abd49766476c9b85792c3b8ce3
Tien Dat <dotiend3@gmail.com>
2025-06-06 10:20:35 +0700

    update giao diện lần chấm

--- 5S_APP/src/app/tabs/survey/strore/check-list/check-list.component.ts
+++ 5S_APP/src/app/tabs/survey/strore/check-list/check-list.component.ts
@@ -11,10 +11,38 @@ import { Router } from '@angular/router';
   templateUrl: './check-list.component.html',
   styleUrls: ['./check-list.component.scss'],
 })
-export class CheckListComponent  implements OnInit {
-
-  constructor() { }
+export class CheckListComponent implements OnInit {
+  fakeApi: any[] = [
+    {
+      lanCham: 'Lần chấm thứ 1',
+      diem: '100 điểm',
+      nguoiCham: 'Lê Thị Lệ Thuý',
+      date: '07-03-2025 07:53',
+    },
+    {
+      lanCham: 'Lần chấm thứ 2',
+      diem: '99 điểm',
+      nguoiCham: 'Nguyễn Thị Lân CH17',
+      date: '12-03-2025 07:53',
+    },
+    {
+      lanCham: 'Lần chấm thứ 3',
+      diem: '99 điểm',
+      nguoiCham: 'Lê Thị Lệ Thuý',
+      date: '18-03-2025 07:53',
+    },
+    {
+      lanCham: 'Lần chấm thứ 4',
+      diem: '100 điểm',
+      nguoiCham: 'Nguyễn Thị Lân CH17',
+      date: '25-03-2025 07:53',
+    },
+  ];
+  constructor(private router: Router) {}
 
   ngOnInit() {}
+  navigateTo() {
 
+    this.router.navigate([`survey/store/evaluate`]);
+  }
 }
