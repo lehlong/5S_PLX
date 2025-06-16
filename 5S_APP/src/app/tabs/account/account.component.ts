34e50137b04188b0db8d1cd76a9e2cf5f44ee905
Tien Dat <dotiend3@gmail.com>
2025-06-16 09:42:05 +0700

    update

--- 5S_APP/src/app/tabs/account/account.component.ts
+++ 5S_APP/src/app/tabs/account/account.component.ts
@@ -3,6 +3,11 @@ import { Router } from '@angular/router';
 import { StorageService } from 'src/app/service/storage.service';
 import { SharedModule } from 'src/app/shared/shared.module';
 
+interface UserInfo {
+  fullName: string;
+  phoneNumber: string;
+  email: string;
+}
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
@@ -11,21 +16,34 @@ import { SharedModule } from 'src/app/shared/shared.module';
   standalone: true,
 })
 export class AccountComponent implements OnInit {
+  userInfo: UserInfo = {
+    fullName: '',
+    phoneNumber: '',
+    email: '',
+  };
   constructor(
     private router: Router,
-    private _storageService: StorageService,
+    private _storageService: StorageService
+  ) {}
 
-  ) { }
-
-  ngOnInit() { }
+  ngOnInit() {
+    this.loadUserInfo();
+  }
   navigateTo(route: string) {
     this.router.navigate([route]);
   }
 
-
-  Logout() {
-    localStorage.clear()
-    this.router.navigate(['/login'])
+  loadUserInfo() {
+    const userInfoString = localStorage.getItem('UserInfo');
+    if (userInfoString) {
+      const userInfo = JSON.parse(userInfoString);
+      console.log(userInfo);
+      this.userInfo = userInfo
+    }
   }
 
+  Logout() {
+    localStorage.clear();
+    this.router.navigate(['/login']);
+  }
 }
--- 5S_APP/src/app/tabs/account/page/terms-policies/terms-policies.component.ts
+++ 5S_APP/src/app/tabs/account/page/terms-policies/terms-policies.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @Component({
   selector: 'app-terms-policies',
   templateUrl: './terms-policies.component.html',
   styleUrls: ['./terms-policies.component.scss'],
+  standalone: true,
+  imports: [SharedModule]
 })
 export class TermsPoliciesComponent  implements OnInit {
 
