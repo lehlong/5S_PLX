d7b3d9a7f380c671b8174041bd417a584e3e6200
thi2000 <62785764+thi2000@users.noreply.github.com>
2025-06-17 18:06:32 +0700

    update

---
+++ 5S_API/5S.CORE/Entities/MD/TblMdDevice.cs
@@ -0,0 +1,44 @@
+﻿
+using PLX5S.CORE.Common;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PLX5S.CORE.Entities.MD
+{
+    [Table("T_MD_DEVICE")]
+    public class TblMdDevice : SoftDeleteEntity
+    {
+        [Key]
+        [Column("ID")]
+        public string Id { get; set; }
+
+        [Column("USERNAME")]
+        public string UserName { get; set; }
+
+        [Column("DEVICE_ID")]
+        public string DeviceId { get; set; }
+
+        [Column("DEVICE_NAME")]
+        public string DeviceName { get; set; }
+
+        [Column("OPERATINGSYSTEM")]
+        public string OperatingSystem { get; set; }
+
+        [Column("MODEL")]
+        public string Model { get; set; }
+
+        [Column("MANUFACTURER")]
+        public string Manufacturer { get; set; }
+
+        [Column("OSVERSION")]
+        public string OsVersion { get; set; }
+        [Column("MAIN_DEVICE")]
+        public bool MainDevice { get; set; }
+        [Column("ENABLE_LOGIN")]
+        public bool EnableLogin { get; set; }
+
+
+
+    }
+}
