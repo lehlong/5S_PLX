02b8e4e76ff18956ec19981edadfe4ee7a045761
Tien Dat <dotiend3@gmail.com>
2025-08-22 15:00:46 +0700

    update

--- 5S_APP/src/app/layout/main-layout/main-layout.component.ts
+++ 5S_APP/src/app/layout/main-layout/main-layout.component.ts
@@ -1,17 +1,33 @@
 import { Component, EnvironmentInjector, inject, OnInit } from '@angular/core';
 import { SharedModule } from '../../shared/shared.module';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-main-layout',
   templateUrl: './main-layout.component.html',
   styleUrls: ['./main-layout.component.scss'],
   imports: [SharedModule],
-  standalone: true
+  standalone: true,
 })
 export class MainLayoutComponent implements OnInit {
+  showFooter = true;
   public environmentInjector = inject(EnvironmentInjector);
 
-  constructor() {}
+  constructor(private router: Router) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        // check url
+        if (
+          event.urlAfterRedirects.startsWith('/survey/evaluate/draft') ||
+          event.urlAfterRedirects.startsWith('/survey/check-list')
+        ) {
+          this.showFooter = false;
+        } else {
+          this.showFooter = true;
+        }
+      }
+    });
+  }
 }
